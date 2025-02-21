bee0405df1c564863a890c284a91c338210fd078
Jerson Steven Rozo Garcia <1025527176@u.icesi.edu.co>
2025-02-21 12:59:39 -0500

    entrega

--- src/ui/Atlas.java
+++ src/ui/Atlas.java
@@ -1,8 +1,8 @@
 /*
 * Analisis
-* Descripcion del programa:
-* Entradas:
-* Salidas: 
+* Descripcion del programa: Programa para calcular costos de paquete 
+* Entradas: 
+* Salidas: costos de viaje, costos de hospedaje, total a pagar
 * Ejemplo:
 */
 
@@ -15,7 +15,9 @@ public class Atlas {
 	private Scanner escaner;
 
     // Declaracion de constantes (completar)
+    
     final double COSTO_TRAYECTO_AVION = 250000;
+    final double COSTO_CADA_NOCHE_CON_ATALAS = 150000;
 
 	private Atlas() {
 		escaner = new Scanner(System.in);
@@ -26,11 +28,20 @@ public class Atlas {
 	{   
         // Declaracion de info a pedir del usuario (completar)
         String nombre;
+        int edad, documento,noches_de_estadia; 
         // Saludo
         System.out.println("Bienvenido al programa de viajes EVENTOS ATLAS realizado por los estudiantes de Algoritmos y programacion I de la Universidad ICESI.");
         // Pedir info al usuario (completar)
         System.out.println("Cual es tu nombre?");
         nombre = escaner.nextLine();
+          System.out.println("Cual es tu edad?");
+        edad = escaner.nextInt();
+             System.out.println("Digita tu numero de documento");
+        documento = escaner.nextInt();
+              System.out.println("Desde que ciudad te estas comunicando?");
+        nombre = escaner.nextLine();
+              System.out.println("Cuantas noches piensas hospedarte?");
+        noches_de_estadia = escaner.nextInt();
 
         // ...
         System.out.println("Bienvenido " + nombre + "!");
@@ -38,13 +49,21 @@ public class Atlas {
 
         // Declaracion de salidas calculadas (completar)
         double totalTransporte;
+        int vuelo_de_ida;
+        int vuelo_de_vuelta;
         // Calculo de salidas mediante metodos (completar)
-        totalTransporte = calcularTotalTransporte(); // aqui podrian faltar parametros
+        totalTransporte = calcularTotalTransporte(vuelo_de_ida, vuelo_de_vuelta); // aqui podrian faltar parametros
 	}
 
 	public static void main(String[] args) {
 		Atlas mainApp = new Atlas();
 		mainApp.run();
+       
+	}
+    public int calcularTotalTransporte(int vuelo_de_ida, vuelo_de_vuelta){
+        return vuelo_de_ida + vuelo_de_vuelta;
+    }
+       
 	}
 
     // Completar metodos para calcular salidas
